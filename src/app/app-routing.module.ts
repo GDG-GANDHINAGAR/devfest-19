18dcbaf5674d811c1c06e63eeb3cfa48731263af
Aditya Prakash <42460356+pAditya198@users.noreply.github.com>
2019-08-08 00:59:04 +0530

    updated app router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { SpeakersPageComponent } from './components/speakers-page/speakers-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomePageComponent }
+  { path: 'home', component: HomePageComponent },
+  { path: 'speakers', component: SpeakersPageComponent },
 ];
 
 @NgModule({

3527d5cfdd9de6de4c1fce3f433514433ed4459c
Aditya Prakash <p.aditya.198@gmail.com>
2019-08-13 19:06:14 +0530

    Team config model created and added

---
+++ src/app/models/team.model.ts
@@ -0,0 +1,21 @@
+
+export interface TeamConfig {
+    name: String;
+    role: String;
+    socialMediaLinks: SocialMediaObject[];
+    image: string;
+}
+
+export interface SocialMediaObject {
+    link: String;
+    kind: SocialMediaKind;
+}
+
+export enum SocialMediaKind {
+    github = 'Github',
+    linkedin = 'Linked In',
+    insta = 'InstaGram',
+    fb = 'Facebook',
+    twitter = 'Twitter',
+    Stackoverflow = 'Stackoverflow'
+}

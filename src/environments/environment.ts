5d5da5ee90ff090ff5a76dcc73cffd9de937a1ca
Pushkar Patel <thepushkarp@gmail.com>
2019-09-02 21:02:48 +0530

    Made changes to environment.prod instead of environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,17 +2,11 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { config } from '../../util/keyConfig';
+
 export const environment = {
   production: false,
-  firebaseConfig : {
-    apiKey: '<API_KEY>',
-    authDomain: '<PROJECT_ID>.firebaseapp.com',
-    databaseURL: 'https://<DATABASE_NAME>.firebaseio.com',
-    projectId: '<PROJECT_ID>',
-    storageBucket: '<BUCKET>.appspot.com',
-    messagingSenderId: '<SENDER_ID>',
-    appId: '<APP_ID>'
-  }
+  firebaseConfig : config
 };
 
 /*

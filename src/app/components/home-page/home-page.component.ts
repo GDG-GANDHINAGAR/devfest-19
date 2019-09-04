a30285395b04ef9b19986cc02c5f3bf9fd45b44c
parth181195 <parthjansari@outlook.com>
2019-09-05 01:02:26 +0530

    started integrating firestore

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,14 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { AngularFirestore } from '@angular/fire/firestore';
 import { config } from 'util/keyConfig';
 import { Observable } from 'rxjs';
 import { HomePageData, PreviousSpeakers, Speakers } from 'src/app/models/interfaces';
-=======
-import { HomepageConfig } from 'src/app/models/homepage.model';
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
-import { Observable } from 'rxjs/Observable';
->>>>>>> 5b493e0c9e4e0bd7fbd8a58032344573e9639262
 
 @Component({
   selector: 'app-home-page',
@@ -16,7 +10,6 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./home-page.component.sass']
 })
 export class HomePageComponent implements OnInit {
-<<<<<<< HEAD
   homePageData: HomePageData = <HomePageData>{};
   previousSpeakers: PreviousSpeakers = <PreviousSpeakers>{};
   speakers: Speakers = <Speakers>{};
@@ -32,15 +25,6 @@ export class HomePageComponent implements OnInit {
       this.speakers = data;
     });
 
-  }
-=======
-  homepageDocRef: AngularFirestoreDocument<HomepageConfig>;
-  homepage: Observable<HomepageConfig>;
->>>>>>> 5b493e0c9e4e0bd7fbd8a58032344573e9639262
-
-  constructor( private afs: AngularFirestore ) {
-    this.homepageDocRef = this.afs.doc<HomepageConfig>('homepage/event-details');
-    this.homepage = this.homepageDocRef.valueChanges();
   }
   ngOnInit() {
   }

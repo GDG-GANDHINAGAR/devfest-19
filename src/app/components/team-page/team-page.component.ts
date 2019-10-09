3cf8eb984e97b08a5c134b09a18c86495f1c9291
Aman Raj <42104907+AmanRaj1608@users.noreply.github.com>
2019-10-10 00:16:25 +0530

    isMobile fix ✔ (#111)
    * isMobile fix ✔
    
    * padding fix

--- src/app/components/team-page/team-page.component.ts
+++ src/app/components/team-page/team-page.component.ts
@@ -12,33 +12,23 @@ export class TeamPageComponent implements OnInit {
   teamDocRef: AngularFirestoreDocument<TeamModel>;
   team: TeamModel;
   core = [];
-  teams = [];
+
   constructor(private afs: AngularFirestore) {
     this.teamDocRef = this.afs.doc<TeamModel>('team/data');
     this.teamDocRef.valueChanges().subscribe(data => {
       this.team = {
         data: data.data.filter(ele => {
-          // console.log(ele.index, ele.name)
           if (ele.core) {
             this.core.push(ele);
-            this.core.sort((a, b) => (a.index > b.index) ? 1 : -1)
             return false;
           }
-          // console.log(ele)
-          this.teams.push(ele);
-          this.teams.sort((a, b) => (a.index > b.index) ? 1 : -1)
           return true;
         })
       };
       // console.log(this.team);
     });
-
-    console.log(this.core);
   }
-  
-  
   ngOnInit() {
-
   }
 
 }
